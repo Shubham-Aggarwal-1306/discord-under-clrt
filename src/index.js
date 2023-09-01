643d8073e790f522efea73b30a49953b1ab9def7
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-09-01 18:00:20 +0530

    Lecture 6 complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
+const { Client, IntentsBitField, EmbedBuilder, ActivityType } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 const client = new Client({
@@ -10,29 +10,30 @@ const client = new Client({
     ]
 });
 
+let status = [
+    {
+        name: 'with discord.js',
+        type: ActivityType.Streaming,
+        url: 'https://www.twitch.tv/monstercat'
+    },
+    {
+        name: 'Slaying Monsters',
+        type: ActivityType.Playing,
+    },
+    {
+        name: 'Lurking',
+        type: ActivityType.Watching,
+    },
+]
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    setInterval(() => {
+        let randomStatus = status[Math.floor(Math.random() * status.length)];
+        client.user.setActivity(randomStatus);
+    }, 1000);
 });
 
-client.on('interactionCreate', async interaction => {
-    try {
-        if (!interaction.isButton()) return;
-        await interaction.deferReply({ ephemeral: true });
-        const role = interaction.guild.roles.cache.get(interaction.customId);
-        if (!role) throw new Error('Role not found!');
-        const member = interaction.guild.members.cache.get(interaction.user.id);
-        if (!member) throw new Error('Member not found!');
-        if (member.roles.cache.has(role.id)) {
-            await member.roles.remove(role);
-            interaction.editReply({ content: `You have been removed from the ${role.name} role!` });
-        } else {
-            await member.roles.add(role);
-            interaction.editReply({ content: `You have been added to the ${role.name} role!` });
-        }
-    } catch (error) {
-        console.log(error);
-    }
-});
 
 client.login(process.env.DISCORD_TOKEN);
 

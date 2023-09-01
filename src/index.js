49c4d3d7c45fafb0bf1a1029afc9438786da829b
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-09-01 17:52:45 +0530

    Lesson 5 complete

--- src/index.js
+++ src/index.js
@@ -15,33 +15,22 @@ client.on('ready', () => {
 });
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'embed') {
-        const embed = new EmbedBuilder()
-            .setTitle('A slick little embed')
-            .setDescription('Hello, this is a slick embed!').setColor('#0099ff').addFields(
-                { name: 'Regular field title', value: 'Some value here' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-            ).setTimestamp();
-        await interaction.reply({ embeds: [embed] });
-    }
-});
-
-
-client.on('messageCreate', async message => {
-    if (message.content === 'embed') {
-        const embed = new EmbedBuilder()
-            .setTitle('A slick little embed')
-            .setDescription('Hello, this is a slick embed!').setColor('#0099ff').addFields(
-                { name: 'Regular field title', value: 'Some value here' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-            ).setTimestamp();
-        await message.reply({ embeds: [embed] });
+    try {
+        if (!interaction.isButton()) return;
+        await interaction.deferReply({ ephemeral: true });
+        const role = interaction.guild.roles.cache.get(interaction.customId);
+        if (!role) throw new Error('Role not found!');
+        const member = interaction.guild.members.cache.get(interaction.user.id);
+        if (!member) throw new Error('Member not found!');
+        if (member.roles.cache.has(role.id)) {
+            await member.roles.remove(role);
+            interaction.editReply({ content: `You have been removed from the ${role.name} role!` });
+        } else {
+            await member.roles.add(role);
+            interaction.editReply({ content: `You have been added to the ${role.name} role!` });
+        }
+    } catch (error) {
+        console.log(error);
     }
 });
 
